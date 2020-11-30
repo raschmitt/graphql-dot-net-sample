e8ce3d8548ea0724fb2d465470cad3e3e2172e0c
Rogerio Schmitt <rogerio.schmitt@me.com>
2020-11-30 20:15:25 -0300

    Remove comments

--- GraphQL.Sample/GraphQL.Sample.Api/Startup.cs
+++ GraphQL.Sample/GraphQL.Sample.Api/Startup.cs
@@ -44,13 +44,10 @@ namespace GraphQL.Sample.Api
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                // .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
             
             Configuration = builder.Build();
             
-            // Console.WriteLine($"Running on environment: {env.EnvironmentName}");
-
             var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope();
             var context = ServiceProviderServiceExtensions.GetRequiredService<Context>(serviceScope.ServiceProvider);
             
